bc77c93a3ee3441bf0c5cf64b52dc4e40f640d5c
Phil Spitler (PRS) <phil@bloc.io>
2015-10-01 15:53:22 -0400

    3: Routing and States - Checkpoint & Assignment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,27 @@
-var myAppModule = angular.module('blocJams', []);
+var myAppModule = angular.module('blocJams', ['ui.router']);
+
+myAppModule.config(function($stateProvider, $locationProvider) {
+
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+
+  $stateProvider.state('landing', {
+    url: '/landing',
+    // controller: 'Album.controller',
+    templateUrl: '/templates/landing.html'
+  });
+
+  $stateProvider.state('album', {
+    url: '/album',
+    // controller: 'Album.controller',
+    templateUrl: 'templates/album.html'
+  });
+
+  $stateProvider.state('collection', {
+    url: '/collection',
+    // controller: 'Album.controller',
+    templateUrl: 'templates/collection.html'
+  });
+});
